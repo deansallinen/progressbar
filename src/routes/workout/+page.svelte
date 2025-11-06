0f94c9305d58bc7bff3afdd041e77e3d3d15cb8c
Dean Sallinen <7519573+deansallinen@users.noreply.github.com>
2025-11-05 16:08:52 -0800

    Add sections to workout page

--- src/routes/workout/+page.svelte
+++ src/routes/workout/+page.svelte
@@ -27,57 +27,59 @@
 		<hr />
 		{#each workout.exercises as exercise, exerciseIndex}
 			{#if exercise}
-				<div class="flex gap-2 items-baseline">
-					<h2>{exercise.name}</h2>
-					<TargetWeight {exercise} />
-				</div>
 				{@const userExercise = await getExerciseById(exercise.exerciseId)}
-				{#if userExercise}
-					<ExerciseProgress exercise={userExercise} />
-				{/if}
-				<table>
-					<thead>
-						<tr class="">
-							<th>Set</th>
-							<th>Target</th>
-							<th style="text-align:right;">Completed Reps</th>
-						</tr>
-					</thead>
-					<tbody>
-						{#if exercise.sets}
-							{#each exercise.sets as set, setIndex}
-								{#if set}
-									<tr>
-										<td class="align-top">{setIndex + 1}</td>
-										<td class="align-top">
-											<div class="">
-												<span class="text-nowrap">
-													<strong class="text-xl">{set.targetWeight}</strong>
-													{#if $settings}
-														{$settings.weightUnit}
+				<section>
+					<div class="flex gap-2 items-baseline">
+						<h2>{exercise.name}</h2>
+						<TargetWeight {exercise} />
+					</div>
+					{#if userExercise}
+						<ExerciseProgress exercise={userExercise} />
+					{/if}
+					<table>
+						<thead>
+							<tr class="">
+								<th>Set</th>
+								<th>Target</th>
+								<th style="text-align:right;">Completed Reps</th>
+							</tr>
+						</thead>
+						<tbody>
+							{#if exercise.sets}
+								{#each exercise.sets as set, setIndex}
+									{#if set}
+										<tr>
+											<td class="align-top">{setIndex + 1}</td>
+											<td class="align-top">
+												<div class="">
+													<span class="text-nowrap">
+														<strong class="text-xl">{set.targetWeight}</strong>
+														{#if $settings}
+															{$settings.weightUnit}
+														{/if}
+													</span>
+													{#if !set.completedAt}
+														<PlateCalculator weight={set.targetWeight} />
 													{/if}
-												</span>
-												{#if !set.completedAt}
-													<PlateCalculator weight={set.targetWeight} />
-												{/if}
-											</div>
-										</td>
-										<td class="align-top">
-											<CompleteSetButton
-												{set}
-												onCompleteSet={(reps) =>
-													completeSet(exerciseIndex, setIndex, reps)}
-											/>
-										</td>
-									</tr>
-									{#if set.completedAt && setIndex < exercise.sets.length - 1 && !exercise.sets[setIndex + 1].completedAt}
-										<RestTimer lastCompletedAt={set.completedAt} />
+												</div>
+											</td>
+											<td class="align-top">
+												<CompleteSetButton
+													{set}
+													onCompleteSet={(reps) =>
+														completeSet(exerciseIndex, setIndex, reps)}
+												/>
+											</td>
+										</tr>
+										{#if set.completedAt && setIndex < exercise.sets.length - 1 && !exercise.sets[setIndex + 1].completedAt}
+											<RestTimer lastCompletedAt={set.completedAt} />
+										{/if}
 									{/if}
-								{/if}
-							{/each}
-						{/if}
-					</tbody>
-				</table>
+								{/each}
+							{/if}
+						</tbody>
+					</table>
+				</section>
 			{/if}
 		{/each}
 		<button onclick={completeWorkout}>Complete Workout</button>
